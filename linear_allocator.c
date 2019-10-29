3df3eccd9d48c5ea6103ac73bb873031bc221dc1
VladimirBalun <balun2010@mail.ru>
2019-10-29 21:19:19 +0300

    feat: add linear allocator
    Was added linearallocator implementation with common functions.

---
+++ linear_allocator.c
@@ -0,0 +1,80 @@
+#include "linear_allocator.h"
+#include "tools.h"
+
+#include <stdint.h>
+#include <string.h>
+
+int la_create(linear_allocator_t* p_allocator, const size_t sz_memory_size, const size_t sz_alignment)
+{
+    if (!p_allocator)
+        return FAIL_RESULT;
+
+    if ( (sz_memory_size == 0u) || (!IS_EVEN_UNSIGNED_NUMBER(sz_alignment)) || (sz_memory_size < sz_alignment) )
+        return FAIL_RESULT;
+
+    p_allocator->p_memory_pointer = malloc(sz_memory_size);
+    if (!p_allocator->p_memory_pointer)
+        return FAIL_RESULT;
+
+    p_allocator->sz_memory_used_size = 0u;
+    p_allocator->sz_alignment = sz_alignment;
+    p_allocator->sz_memory_total_size = sz_memory_size;
+
+#ifdef LA_WITH_ZEROING
+    memset(p_allocator->p_memory_pointer, 0, p_allocator->sz_memory_total_size);
+#endif // WITH_ZEROING
+
+    return SUCCESSFUL_RESULT;
+}
+
+void* la_allocate(linear_allocator_t* p_allocator, const size_t sz_allocated_size)
+{
+    if (!p_allocator)
+        return NULL;
+
+    const size_t sz_alignment = p_allocator->sz_alignment;
+    const size_t sz_used_size = p_allocator->sz_memory_used_size;
+    const size_t sz_total_size = p_allocator->sz_memory_total_size;
+    const uint8_t* p_memory_pointer = (const uint8_t*)p_allocator->p_memory_pointer;
+
+    if (sz_total_size - sz_used_size < sz_allocated_size)
+        return NULL;
+
+    if (sz_alignment == 0u)
+    {
+        p_allocator->sz_memory_used_size += sz_allocated_size;
+        void* p_allocated_area = (void*)((size_t)p_memory_pointer + sz_used_size);
+        return p_allocated_area;
+    }
+    else
+    {
+        // TODO: need to implement allocation with alignment
+        p_allocator->sz_memory_used_size += sz_allocated_size;
+        void* p_allocated_area = (void*)((size_t)p_memory_pointer + sz_used_size);
+        return p_allocated_area;
+    }
+}
+
+void la_clean(linear_allocator_t* p_allocator)
+{
+    if (!p_allocator)
+        return;
+
+    p_allocator->sz_memory_used_size = 0u;
+
+#ifdef LA_WITH_ZEROING
+    memset(p_allocator->p_memory_pointer, 0, p_allocator->sz_memory_total_size);
+#endif // WITH_ZEROING
+}
+
+void la_destroy(linear_allocator_t* p_allocator)
+{
+    if (!p_allocator)
+        return;
+
+    free(p_allocator->p_memory_pointer);
+    p_allocator->p_memory_pointer = NULL;
+    p_allocator->sz_memory_total_size = 0u;
+    p_allocator->sz_memory_used_size = 0u;
+    p_allocator->sz_alignment = 0u;
+}
---
+++ linear_allocator.h
@@ -0,0 +1,82 @@
+#ifndef LINEAR_ALLOCATOR_H
+#define LINEAR_ALLOCATOR_H
+
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/*! 
+ * \brief Linear allocator implementation.
+ * 
+ * This is the simplest kind of allocator. The idea is to keep a pointer at the first 
+ * memory address of your memory area and move it every time an allocation is done. 
+ * In this allocator, the internal fragmentation is kept to a minimum because all elements 
+ * are sequentially (spatial locality) inserted and the only fragmentation between them 
+ * is the alignment(that can be disabled).
+ * 
+ * Complexity:
+ *  - allocation: O(1)
+ *  - free:       absent in this allocator, all memory is freed together.
+ *  - clean:      O(1)
+ *  
+ *  If the 'LA_WITH_ZEROING' was defined, then linear allocator will use 
+ *  zeroing of the unused memory(useful for debugging).
+ */
+typedef struct linear_allocator {
+    void*  p_memory_pointer;
+    size_t sz_memory_total_size;
+    size_t sz_memory_used_size;
+    size_t sz_alignment;
+} linear_allocator_t;
+
+/*!
+ * Function for initialization of the linear allocator.
+ * This function is safe for NULL value of the 'p_allocator'.
+ * 
+ * \param[in] p_allocator Pointer on the linear allocator structure.
+ * \param[in] sz_memory_size Total memory size for linear allocator(size is fixed).
+ * \param[in] sz_alignment Alignment for each allocation, if 'sz_alignment' is equal 0, then
+ * linear works without alignment of the its elements.
+ * \return Returns 1, if the linear allocator was initialized successfully, otherwise returns 0(the linear allocator
+ * will not be initialize, if 'sz_memory_size' is equal 0 or 'sz_alignment' is not even or
+ * 'sz_memory_size' is less than 'sz_alignment' or system was not allocated necessary memory for allocator).
+ */
+int la_create(linear_allocator_t* p_allocator, size_t sz_memory_size, size_t sz_alignment);
+
+/*!
+ * Function for allocation memory area from the linear allocator.
+ * This function is safe for NULL value of the 'p_allocator'.
+ *
+ * \warning NULL can be returned.
+ * \param[in] p_allocator Pointer on the linear allocator structure.
+ * \param[in] sz_allocated_size Size of the allocated memory area.
+ * \return Returns allocated memory area, if the allocation was successfully, otherwise
+ * returns NULL.
+ */
+void* la_allocate(linear_allocator_t* p_allocator, size_t sz_allocated_size);
+
+/*!
+ * Function for cleaning all the elements of the linear allocator,
+ * after cleaning the linear allocator will be valid for using again.
+ * This function is safe for NULL value of the 'p_allocator'.
+ *
+ * \param[in] p_allocator Pointer on the linear allocator structure.
+ */
+void la_clean(linear_allocator_t* p_allocator);
+
+/*!
+ * Function for destroying of the linear allocator, after
+ * destroying the linear allocator will not be valid for using again.
+ * This function is safe for NULL value of the 'p_allocator'.
+ *
+ * \param[in] p_allocator Pointer on the linear allocator structure.
+ */
+void la_destroy(linear_allocator_t* p_allocator);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // LINEAR_ALLOCATOR_H
